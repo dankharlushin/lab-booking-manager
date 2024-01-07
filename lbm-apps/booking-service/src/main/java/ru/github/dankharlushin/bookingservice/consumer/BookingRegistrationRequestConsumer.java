4ec482db9d3a789c3b57d29ba70ee7f71e09ab7d
DanilKharlushin <danilharlushin12@gmail.com>
2024-01-07 10:12:38 +0300

    Add retries for booking_registration topic

--- lbm-apps/booking-service/src/main/java/ru/github/dankharlushin/bookingservice/consumer/BookingRegistrationRequestConsumer.java
+++ lbm-apps/booking-service/src/main/java/ru/github/dankharlushin/bookingservice/consumer/BookingRegistrationRequestConsumer.java
@@ -3,6 +3,8 @@ package ru.github.dankharlushin.bookingservice.consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.DltStrategy;
 import org.springframework.stereotype.Component;
 import ru.github.dankharlushin.bookingservice.handler.BookingRequestHandler;
 import ru.github.dankharlushin.lbmlib.data.dto.BookingRegistrationRequest;
@@ -19,7 +21,11 @@ public class BookingRegistrationRequestConsumer {
     }
 
     @KafkaListener(topics = "${booking-service.registration.kafka.topic.name}",
-            containerFactory = "bookingRegistrationKafkaListenerContainerFactory")
+            containerFactory = "bookingRegistrationKafkaListenerContainerFactory"
+    )
+    @RetryableTopic(
+            dltStrategy = DltStrategy.FAIL_ON_ERROR
+    )
     public void bookingRegistrationListener(final BookingRegistrationRequest registrationRequest) {
         logger.info("Consuming registration request [{}]", registrationRequest);
         requestHandler.handleRegistration(registrationRequest);
